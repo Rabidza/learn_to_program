7f831bbc9c71edd4b1b9cd995e95e8260955fe12
Neill Henning <neillhenning@gmail.com>
2019-05-15 08:06:23 +0200

    Finish 99 bottles

---
+++ chapter06/loopLogic.rb
@@ -0,0 +1,18 @@
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+if name == 'Chris'
+  puts 'What a lovely name!'
+else
+  if name == 'Katy'
+    puts 'What a lovely name!'
+  end
+end
+
+# Applying the DRY (Do not Repeat Yourself)
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+if (name == 'Chris' or name == 'Katy')
+  puts 'What a lovely name!'
+end
