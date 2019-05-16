cc00e094061f73ccef38f0c2a89d44f531816464
Neill Henning <neillhenning@gmail.com>
2019-05-16 08:16:30 +0200

    Start Chapter07

---
+++ chapter07/arrays.rb
@@ -0,0 +1,14 @@
+[]
+[5]
+['Hello', 'Goodbye']
+flavor = 'vanilla'             # This is not an array, of course...
+[89.9, flavor, [true, false]]  # ...but this is.
+
+
+
+names = ['Ada', 'Belle', 'Chris']
+puts names
+puts names[0]
+puts names[1]
+puts names[2]
+puts names[3]  # This is out of range.
