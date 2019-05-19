062322507d7c271b68c7ca2b38a062ef3bdde633
Neill Henning <neillhenning@gmail.com>
2019-05-19 19:20:12 +0200

    Update Progress

--- chapter09/happyBirthday.rb
+++ chapter09/happyBirthday.rb
@@ -3,3 +3,16 @@ Happy Birthday!
 Ask what year a person was born in, then the month, then the day. 
 Figure out how old they are and give them a big SPANK! for each birthday they have had.
 =end
+puts "Enter the year you were born in:"
+year = gets.chomp
+puts "Enter the month:"
+month = gets.chomp
+puts "Enter the day:"
+day = gets.chomp
+
+born =  Time.mktime(year, month, day)  # When I was born.
+today = Time.new
+
+num_birthdays = (today.year - born.year) 
+
+puts "SPANK!\n" * num_birthdays
